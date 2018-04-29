6ae63743edae45dea18e6af6bbdd777ff4d2ecbe
PABLO-LAPTOP\pablo <pabloandresboada@gmail.com>
2018-04-29 15:44:25 -0500

    first commit

--- Autoscaling_dynos.py
+++ Autoscaling_dynos.py
@@ -8,10 +8,11 @@ print("**************************************************")
 
 access_id=os.environ["S3_AWS_ACCESS_KEY_ID"]
 access_secret=os.environ["S3_AWS_SECRET_ACCESS_KEY"]
+sqs_cliente = boto3.client('s3', aws_access_key_id=access_id, aws_secret_access_key=access_secret)
 sqs = boto3.resource('sqs', aws_access_key_id=access_id, aws_secret_access_key=access_secret)
 queue = sqs.get_queue_by_name(QueueName='sqs_concursos')
 url_queue=queue.url
 print("Conectado a la url: ", url_queue)
 while True:
-    total_mensajes=queue.ApproximateNumberOfMessages
+    total_mensajes=sqs_cliente.ApproximateNumberOfMessages
     print(total_mensajes)
