cf1437b839618bf310db891a00793cef60c38093
PABLO-LAPTOP\pablo <pabloandresboada@gmail.com>
2018-04-29 16:32:27 -0500

    identificando limite de mensajes y leyendo mensajes en la cola

--- Autoscaling_dynos.py
+++ Autoscaling_dynos.py
@@ -9,6 +9,8 @@ print("**************************************************")
 
 print("Digite el numero de mensajes limite para escalar el dyno del worker")
 limite_mensajes=input()
+num_max_dinos=2
+dynos_iniciales=1
 
 while True:
     access_id=os.environ["S3_AWS_ACCESS_KEY_ID"]
@@ -21,13 +23,22 @@ while True:
 
     total_mensajes=queue.attributes
     #print(total_mensajes["ApproximateNumberOfMessages"])
-    if int(total_mensajes["ApproximateNumberOfMessages"])>int(limite_mensajes):
-        print("ALERTA!! se sobrepaso el limite de mensajes en la cola")
+    if int(total_mensajes["ApproximateNumberOfMessages"])>int(limite_mensajes) and dynos_iniciales<num_max_dinos:
+        print("ALERTA!!!!!!! se sobrepaso el limite de mensajes en la cola")
         print("Debe escalarse el dyno del Worker!!!!!!!!!")
-    else:
-        print("No se ha sobrepasado el limite de mensajes")
+        print("Ejecutanto sentencia heroku")
+        os.system('heroku ps:scale --app "salty-wave-46611" worker=2')
+        print("Dyno escalado, revisar!!!!!")
+        dynos_iniciales=dynos_iniciales+1
+    if int(total_mensajes["ApproximateNumberOfMessages"])<int(limite_mensajes):
+        print("Limite normal de mensajes")
         print("Maximo numero de mensajes elegido: ", limite_mensajes )
         print("Mensajes en la cola: ", int(total_mensajes["ApproximateNumberOfMessages"]))
+        if dynos_iniciales == 2:
+            print("Se debe decrementar 1 dyno")
+            os.system('heroku ps:scale --app "salty-wave-46611" worker=1')
+            dynos_iniciales=dynos_iniciales-1
+
     time.sleep(5)
     print("**************************************************")
     print("**************************************************")
